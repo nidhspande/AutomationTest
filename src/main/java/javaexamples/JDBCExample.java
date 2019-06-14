88c135300eacacdc329afde8ffbbc898ba175fc0
pande <pande@DESKTOP-719K3UV.fios-router.home>
2019-06-13 21:16:56 -0400

    Upadate

--- src/main/java/javaexamples/JDBCExample.java
+++ src/main/java/javaexamples/JDBCExample.java
@@ -10,14 +10,26 @@ public class JDBCExample {
 
 	public static void main(String[] args) {
 
+		
+		// Get Connection String From Developer 
 		String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Northwind;user=adminuser;password=123";
 
+		
+		// Open the connection 
 		try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+			
+			
+			//Write the Sql Statement 
+			
 			String SQL = "SELECT TOP (1) ContactName\r\n" + 
 					"  FROM [Northwind].[dbo].[Customers]\r\n" + 
 					"  where City ='London'";
+			
+			// Create a Result set 
 			ResultSet rs = stmt.executeQuery(SQL);
 			while (rs.next()) {
+				
+				// Print the result 
 				System.out.println(rs.getString("ContactName"));
 
 			}
