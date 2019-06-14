eec02b5f85eb5a582cfecab0a3e7fff5b1393535
pande <pande@DESKTOP-719K3UV.fios-router.home>
2019-06-13 20:00:43 -0400

    updated

--- src/main/java/javaexamples/JDBCExample.java
+++ src/main/java/javaexamples/JDBCExample.java
@@ -10,22 +10,21 @@ public class JDBCExample {
 
 	public static void main(String[] args) {
 
-		  String connectionUrl =
-	                 "jdbc:sqlserver://localhost:1433;databaseName=Northwind;user=adminuser;password=123";
+		String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Northwind;user=adminuser;password=123";
 
-	         try (Connection con = DriverManager.getConnection(connectionUrl);
-	                 Statement stmt = con.createStatement();) {
-	             String SQL = "SELECT TOP 10 * FROM Customers;";
-	             ResultSet rs = stmt.executeQuery(SQL);
-	             while (rs.next()) {
-	                 System.out.println(rs.getString("ContactName"));
-	             }
-	         } catch (SQLException e) {
-	             e.printStackTrace();
-	         }
-		
-		
-		
+		try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+			String SQL = "SELECT TOP (1) ContactName\r\n" + 
+					"  FROM [Northwind].[dbo].[Customers]\r\n" + 
+					"  where City ='London'";
+			ResultSet rs = stmt.executeQuery(SQL);
+			while (rs.next()) {
+				System.out.println(rs.getString("ContactName"));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
